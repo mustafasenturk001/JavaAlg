b856353f6df889f16806529514f225e9058a0487
Mustafa Şentürk <93072105+mustafasenturk001@users.noreply.github.com>
2024-11-12 23:37:27 -0600

    test
    test

---
+++ src/CheckPalindrome.java
@@ -0,0 +1,20 @@
+public class CheckPalindrome {
+
+    public static boolean checkPalindromeString(String input) {
+        boolean result = true;
+        int length = input.length();
+
+        for (int i = 0; i < length/2; i++) {
+            if (input.charAt(i) != input.charAt(length - i - 1)) {
+                result = false;
+                break;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(checkPalindromeString("kaya"));
+    }
+}
